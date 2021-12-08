a6cb4104c35635334658970646cfbbfa34a821a5
mwarddev <martinward1981@googlemail.com>
2021-12-08 04:14:42 +0000

    Populate map.js

--- map.js
+++ map.js
@@ -5,16 +5,30 @@
  */
 
 // Using a for loop
-
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for(let num of nums) {
+  results.push(num * 2);
+}
+console.log(results);
 
 // Using map()
-
+const multiByTwo = function(num) {
+  return num * 2;
+}
+const mapResults = nums.map(multiByTwo);
+console.log(mapResults);
 
 // Simplified w/ map()
-
+// Simplify attempt
+console.log(nums.map(n => n * 2));
+// Instructor's simplify
+const mapResults2 = nums.map(function(num) {return num * 2});
+console.log(mapResults2);
 
 // Simplfied w/ map() + arrow function
-
+const arrow = nums.map(n => n * 2);
+console.log(arrow);
 
 // With objects:
 const students = [
@@ -37,3 +51,9 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map(student => [student.name, student.id]);
+console.log(studentsWithIds);
+
+const studentObjects = students.map(student => ({name: student.name, id: student.id, skill: student.skill}));
+console.log(studentObjects);
